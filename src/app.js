de7432367093e2dc943cafcbf437e4f8e70c3154
Dani <denisedanielasartori@gmail.com>
2024-06-03 19:33:23 -0300

    1

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from 'express' 
+
+const app = express()
+
+app.use(express.json())
+
+const selecoes =[
+    {id:1, selecao: 'Brasil', grupo: 'G'},
+    {id:2, selecao: 'Suiça', grupo: 'G'},
+    {id:3, selecao: 'Sérvia', grupo: 'G'},
+    {id:4, selecao: 'Camarões', grupo: 'G'},
+]
+
+app.get('/', (req, res) => { 
+    res.send('Olá Mundo!')
+})
+
+app.get('/selecoes', (req, res) =>{
+    res.status(200).send(selecoes)
+})
+
+app.post('/selecoes', (req, res) => {
+    selecoes.push(req.body)
+    res.status(201).send('Seleção cadastrada com sucesso!')
+})
+
+export default app
