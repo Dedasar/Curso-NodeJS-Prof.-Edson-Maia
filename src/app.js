b2841f4f19794e66b92d9383f599f0411e9fdff7
Dani <denisedanielasartori@gmail.com>
2024-06-16 11:13:45 -0300

    Curso API Rest

--- src/app.js
+++ src/app.js
@@ -1,39 +1,10 @@
-import express from 'express' 
+import express from 'express'
+import routes from './routes.js'
 
 const app = express()
 
 app.use(express.json())
 
-const selecoes =[
-    {id:1, selecao: 'Brasil', grupo: 'G'},
-    {id:2, selecao: 'Suiça', grupo: 'G'},
-    {id:3, selecao: 'Sérvia', grupo: 'G'},
-    {id:4, selecao: 'Camarões', grupo: 'G'},
-]
-
-function buscarSelecaoPorId(id){
-    return selecoes.filter(selecao => selecao.id == id)
-}
-
-function buscarIndexSelecao(id){
-    return selecoes.findIndex(selecao => selecao.id == id)
-}
-
-app.get('/', (req, res) => { 
-    res.send('Olá Mundo!')
-})
-
-app.get('/selecoes', (req, res) =>{
-    res.status(200).send(selecoes)
-})
-
-app.get('/selecoes/:id', (req, res) => {
-    res.json(buscarSelecaoPorId(req.params.id))
-})
-
-app.post('/selecoes', (req, res) => {
-    selecoes.push(req.body)
-    res.status(201).send('Seleção cadastrada com sucesso!')
-})
+app.use(routes)
 
 export default app
